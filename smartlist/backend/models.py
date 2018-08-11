00ba4ff7acb5569f5868a3f2d066747a12bbf389
Fabian Wunsch <fabian@uriah.heep.sax.de>
2018-08-11 17:55:39 +0200

    Added initial DB models

--- smartlist/backend/models.py
+++ smartlist/backend/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=200,
+            help_text="Name of the shoppinglist")
+
+class ProductList(models.Model):
+    name = models.CharField(max_length=200,
+            help_text="Name of the product")
+    product_number = models.CharField(max_length=100,
+            help_text="Short Name or product number")
+    price = models.DecimalField(max_digits=15,
+            decimal_places=2,
+            help_text="Price of the product")
+    category = models.CharField(max_length=100,
+            help_text="Category of the product")
+
+class Entrys(models.Model):
+    shopping_list = models.ForeignKey(ShoppingList,
+            models.CASCADE)
+    product_list = models.ForeignKey(ProductList,
+            models.CASCADE)
+    name = models.CharField(max_length=200,
+            help_text="Name of the list entry")
+    product_number = models.CharField(max_length=100,
+            help_text="Short Name or product number")
+    price = models.DecimalField(max_digits=15,
+            decimal_places=2,
+            help_text="Price of the product")
+    category = models.CharField(max_length=100,
+            help_text="Category of the product")
+    status = models.BooleanField(default=False,
+            help_text="Status of the product")
+
