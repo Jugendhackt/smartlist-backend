b334b4c793a51d0f9546e261e378ab02ce233965
jugendhacker <j.r@wiuwiu.de>
2018-08-11 23:09:42 +0200

    Start to add the rest

---
+++ smartlist/backend/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Product, ShoppingList, Entry
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingList
+        fields = ('name')
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name', 'product_number', 'price', 'category')
+
+
+class EntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Entry
+        fields = ('shopping_list', 'product', 'name', 'price', 'category', 'status')
