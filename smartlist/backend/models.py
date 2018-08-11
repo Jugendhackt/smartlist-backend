1dd5a3837213b798c38c824378c008dfdf67d14b
Fabian Wunsch <fabian@uriah.heep.sax.de>
2018-08-11 18:23:42 +0200

    Renamed ProductList and Entry, Changed status to choices

--- smartlist/backend/models.py
+++ smartlist/backend/models.py
@@ -6,7 +6,7 @@ class ShoppingList(models.Model):
     name = models.CharField(max_length=200,
             help_text="Name of the shoppinglist")
 
-class ProductList(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=200,
             help_text="Name of the product")
     product_number = models.CharField(max_length=100,
@@ -17,20 +17,23 @@ class ProductList(models.Model):
     category = models.CharField(max_length=100,
             help_text="Category of the product")
 
-class Entrys(models.Model):
+class Entry(models.Model):
     shopping_list = models.ForeignKey(ShoppingList,
             models.CASCADE)
-    product_list = models.ForeignKey(ProductList,
+    product = models.ForeignKey(Product,
             models.CASCADE)
     name = models.CharField(max_length=200,
             help_text="Name of the list entry")
-    product_number = models.CharField(max_length=100,
-            help_text="Short Name or product number")
     price = models.DecimalField(max_digits=15,
             decimal_places=2,
             help_text="Price of the product")
     category = models.CharField(max_length=100,
             help_text="Category of the product")
-    status = models.BooleanField(default=False,
+    STATUS = (
+            ("open", "Currently not buyed"),
+            ("closed", "Already bought"),
+    )
+    status = models.CharField(max_length=1,
+            choices=STATUS,
             help_text="Status of the product")
 
