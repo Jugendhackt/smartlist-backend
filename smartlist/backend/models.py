46d01fb45f9972c938069cd3677f18058b0a7a17
Fabian Wunsch <fabian@uriah.heep.sax.de>
2018-08-11 21:04:22 +0200

    Fixed indentation

--- smartlist/backend/models.py
+++ smartlist/backend/models.py
@@ -3,39 +3,49 @@ from django.db import models
 # Create your models here.
 
 class ShoppingList(models.Model):
-    name = models.CharField(max_length=200,
+    name = models.CharField(
+            max_length=200,
             help_text="Name of the shoppinglist")
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=200,
+    name = models.CharField(
+            max_length=200,
             help_text="Name of the product")
-    product_number = models.CharField(max_length=100,
+    product_number = models.CharField(
+            max_length=100,
             help_text="Short Name or product number")
-    price = models.DecimalField(max_digits=15,
+    price = models.DecimalField(
+            max_digits=15,
             decimal_places=2,
             help_text="Price of the product")
-    category = models.CharField(max_length=100,
+    category = models.CharField(
+            max_length=100,
             help_text="Category of the product")
 
     def __str__(self):
         return self.name
 
 class Entry(models.Model):
-    shopping_list = models.ForeignKey(ShoppingList,
+    shopping_list = models.ForeignKey(
+            ShoppingList,
             models.CASCADE)
-    product = models.ForeignKey(Product,
+    product = models.ForeignKey(
+            Product,
             models.CASCADE)
-    name = models.CharField(max_length=200,
+    name = models.CharField(
+            max_length=200,
             null=False, blank=True,
             help_text="Name of the list entry")
-    price = models.DecimalField(max_digits=15,
+    price = models.DecimalField(
+            max_digits=15,
             decimal_places=2,
             null=False, blank=True,
             help_text="Price of the product")
-    category = models.CharField(max_length=100,
+    category = models.CharField(
+            max_length=100,
             null=False, blank=True,
             help_text="Category of the product")
     STATUS = (
