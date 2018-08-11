73164ec409c9c8bb9a4775c97b507889927916e0
Fabian Wunsch <fabian@uriah.heep.sax.de>
2018-08-11 20:30:29 +0200

    Added support for the database at the admin panel

--- smartlist/backend/admin.py
+++ smartlist/backend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, ShoppingList, Entry
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ShoppingList)
+admin.site.register(Entry)
